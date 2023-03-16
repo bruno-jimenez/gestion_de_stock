5a3b0c582f7443f12c579e1094935c1de0803d9a
unknown <bruno-jimenez@laplateforme.io>
2023-03-16 15:20:16 +0100

    adding the file with the main function and start of the UI

--- gestion de stock.py
+++ gestion de stock.py
@@ -36,15 +36,12 @@ def select_categorie():
 root = Tk()
 root.title("Product stock overview")
 
-# Création des widgets pour le tableau de bord
 titre_label = Label(root, text="Product stock")
 ajouter_bouton = Button(root, text="Add product", command=add_product)
 supprimer_bouton = Button(root, text=" Del product ", command=del_product)
 
-# Placement des widgets dans la fenêtre
 titre_label.pack()
 ajouter_bouton.pack()
 supprimer_bouton.pack()
 
-# Boucle principale Tkinter
 root.mainloop()
